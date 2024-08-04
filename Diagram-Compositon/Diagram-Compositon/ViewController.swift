c4a1d97b5d784422397e61fa598f6563a913e929
Thiago Monteiro <thiagomonteiro.developer@gmail.com>
2024-08-03 23:37:09 -0300

    feat(FeedLoader):
    - Create an FeedLoader protocol
    
    - Create a RemoteFeedLoader
    
    - Create a LocalFeedLoader
    
    - Did implements FeedLoader protocol on RemoteFeedLoader and LocalFeedLoader

--- Diagram-Compositon/Diagram-Compositon/ViewController.swift
+++ Diagram-Compositon/Diagram-Compositon/ViewController.swift
@@ -1,19 +1,38 @@
-//
-//  ViewController.swift
-//  Diagram-Compositon
-//
-//  Created by Thiago Monteiro on 03/08/24.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+protocol FeedLoader {
+    func load(completion: @escaping([String]) -> Void)
+}
+
+final class ViewController: UIViewController {
+    private let loader: FeedLoader
+    
+    init(loader: FeedLoader) {
+        self.loader = loader
+        super.init(nibName: nil, bundle: nil)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        loader.load { loadItems in
+            // update
+        }
     }
-
-
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
 
+final class RemoteFeedLoader: FeedLoader {
+    func load(completion: @escaping ([String]) -> Void) {
+        // do something
+    }
+}
+
+final class LocalFeedLoader: FeedLoader {
+    func load(completion: @escaping ([String]) -> Void) {
+        // do something
+    }
+}
